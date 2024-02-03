fb6c3b05c8e5cf2fdfac3f090dd3c303588e3274
Yahor Tsytsarkin <egormail88@gmail.com>
2024-02-03 12:19:04 +0300

    feat: add current working directory

--- initialize.js
+++ initialize.js
@@ -1,13 +1,11 @@
-import { argv } from "node:process";
-
-import { stdin, exit } from "node:process";
+import { stdin, exit, cwd, argv } from "node:process";
 
 export const initialize = () => {
   const args = argv.slice(2);
   const username = args.join().slice(11) || "Guest";
 
   console.log(`Welcome to the File Manager, ${username}!`);
-
+  console.log(`You are currently in, ${cwd()}!`);
   process.on("SIGINT", () => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
     exit();
@@ -15,6 +13,7 @@ export const initialize = () => {
 
   stdin.on("data", (data) => {
     const input = data.toString().trim();
+    console.log(`You are currently in, ${cwd()}!`);
     if (input === ".exit") {
       console.log(`Thank you for using File Manager, ${username}, goodbye!`);
       exit();
