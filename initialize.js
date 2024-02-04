05d951ead0799156818b32486f95bc10b18a3afa
Yahor Tsytsarkin <egormail88@gmail.com>
2024-02-04 16:36:19 +0300

    feat: add up

--- initialize.js
+++ initialize.js
@@ -1,5 +1,6 @@
 import { stdin, exit, cwd, argv } from "node:process";
-import * as os from "node:os";
+import os from "node:os";
+import path from "node:path";
 
 export const initialize = () => {
   const args = argv.slice(2);
@@ -16,10 +17,18 @@ export const initialize = () => {
 
   stdin.on("data", (data) => {
     const input = data.toString().trim();
-    console.log(`You are currently in, ${cwd()}!`);
+
     if (input === ".exit") {
       console.log(`Thank you for using File Manager, ${username}, goodbye!`);
       exit();
+    }
+    if (input === "up") {
+      const currentDir = cwd();
+      const parentDir = path.resolve(currentDir, "..");
+      process.chdir(parentDir);
+      console.log(`You are now in, ${cwd()}!`);
+    } else {
+      console.log(`You are currently in, ${cwd()}!`);
     }
   });
 };
