44b46678052b83343ed371dfdf28dc5647e4b3ee
Yahor Tsytsarkin <egormail88@gmail.com>
2024-02-04 12:37:24 +0300

    feat: add Starting working

--- initialize.js
+++ initialize.js
@@ -1,10 +1,13 @@
 import { stdin, exit, cwd, argv } from "node:process";
+import * as os from "node:os";
 
 export const initialize = () => {
   const args = argv.slice(2);
   const username = args.join().slice(11) || "Guest";
 
   console.log(`Welcome to the File Manager, ${username}!`);
+  process.chdir(os.homedir());
+
   console.log(`You are currently in, ${cwd()}!`);
   process.on("SIGINT", () => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
